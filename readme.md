7697582e281eb4e7d932de53d12ebfd7316be266
JohnM <johnnymoran44@hotmail.com>
2021-10-06 15:50:01 +0200

    set up webpush

--- readme.md
+++ readme.md
@@ -1,3 +1,15 @@
 tips:
 npm i,
 npm start
+
+video:
+https://www.youtube.com/watch?v=HlYFW2zaYQM
+
+using web-push, obtain vapid keys with this command
+`./node_modules/.bin/web-push generate-vapid-keys`
+
+Public Key:
+BCP-MxswepN7VilAen5Qtt6EbT29bOJmt0nfdW06j071j9tSQcYCHFERg_xqKwRlP20jOmmC8huFZwq3UoHosEg
+
+Private Key:
+aW2gi8M4xZVclqAuHQOejvLCNbn1ziOU7IvXw5_bcMA
--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
-import express from 'express';
+import express from "express";
+import { router } from "./router.js";
 
-import { router } from './router.js';
-
+const webpush = require("web-push");
+const path = require("path");
 const app = express();
 const port = 3000;
 
@@ -10,22 +11,36 @@ const port = 3000;
   next();
 }); */
 
-app.get('/images/:image.jpg', (req, res, next) => {
-  // Simulate slow server response
-  setTimeout(
-    () => res.sendFile(`${process.cwd()}/dist/images/${req.params.image}.jpg`),
-    1500
-  );
+const publicVapidKey = "BCP-MxswepN7VilAen5Qtt6EbT29bOJmt0nfdW06j071j9tSQcYCHFERg_xqKwRlP20jOmmC8huFZwq3UoHosEg";
+
+const privateVapidKey = "aW2gi8M4xZVclqAuHQOejvLCNbn1ziOU7IvXw5_bcMA";
+
+webpush.setVapidDetails("mailto:test@test.com", publicVapidKey, privateVapidKey);
+
+//subscribe route
+app.post("/subscribe", (req, res) => {
+	//Get push subscription object
+	const subscription = req.body;
+	res.status(201).json({});
+	//create payload
+	const payload = JSON.stringify({ title: "Push text" });
+	//Pass object to send notification
+	webpush.sendNotification(subscription, payload).catch((err) => console.log(err));
+});
+
+app.get("/images/:image.jpg", (req, res, next) => {
+	// Simulate slow server response
+	setTimeout(() => res.sendFile(`${process.cwd()}/dist/images/${req.params.image}.jpg`), 1500);
 });
 
 app.use(express.static(`${process.cwd()}/dist`));
 
-app.get('/', (req, res) => {
-  res.sendFile(`${process.cwd()}/server/index.html`);
+app.get("/", (req, res) => {
+	res.sendFile(`${process.cwd()}/server/index.html`);
 });
 
-app.use('/api', router);
+app.use("/api", router);
 
 app.listen(port, () => {
-  console.log(`PWA workshp app listening at http://localhost:${port}`);
+	console.log(`PWA workshp app listening at http://localhost:${port}`);
 });
