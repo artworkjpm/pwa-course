9a1a872a5432fa0fed9da49952f9d32f3a976885
JohnM <johnnymoran44@hotmail.com>
2021-10-13 10:04:30 +0200

    xxxx

--- dist/js/register.js
+++ dist/js/register.js
@@ -1,36 +1,53 @@
 console.log("test");
-var swRegistration;
+const publicKey = "BCP-MxswepN7VilAen5Qtt6EbT29bOJmt0nfdW06j071j9tSQcYCHFERg_xqKwRlP20jOmmC8huFZwq3UoHosEg";
+const privateKey = "aW2gi8M4xZVclqAuHQOejvLCNbn1ziOU7IvXw5_bcMA";
 
 //Its important to remember that the root folder is /dist not where the index.html is in /server
 if ("serviceWorker" in navigator) {
-	navigator.serviceWorker.register("service-worker.js").then((e) => {
-		if (e.installing) {
-			console.log("installing");
-		} else if (e.waiting) {
-			console.log("waiting");
-		} else if (e.active) {
-			console.log("active");
-			swRegistration = e;
-		}
-	});
+	serviceWorkerRegistration().catch((err) => console.error(err));
 } else {
 	console.log("Service worker is not supported in your browser");
 }
 
-// async function push(reg) {
-// 	const subscription = await reg.pushManager.subscribe({
-// 		userVisibleOnly: true,
-// 		applicationServerKey: publicVapidKey,
-// 	});
-// 	console.log("push registered");
-// 	//send push notification
-// 	console.log("sending push...");
-// 	await fetch("/subscribe", {
-// 		method: "POST",
-// 		body: JSON.stringify(subscription),
-// 		headers: {
-// 			"content-type": "application/json",
-// 		},
-// 	});
-// 	console.log("push sent....");
-// }
+let register;
+
+document.addEventListener("DOMContentLoaded", () => {
+	document.addEventListener(
+		"click",
+		function (event) {
+			if (!event.target.matches("#subscribe")) return;
+			event.preventDefault();
+			console.log("subscribe me...");
+			fetch("/notify");
+		},
+		false
+	);
+
+	document.addEventListener(
+		"click",
+		async (event) => {
+			if (!event.target.matches("#send-notification")) return;
+			event.preventDefault();
+			console.log("#send-notification");
+			await navigator.serviceWorker.ready;
+
+			const subscription = await register.pushManager.subscribe({
+				userVisibleOnly: true,
+				applicationServerKey: publicKey,
+			});
+
+			await fetch("/subscribe", {
+				method: "POST",
+				body: JSON.stringify(subscription),
+				headers: {
+					"content-type": "application/json",
+				},
+			});
+		},
+		false
+	);
+});
+
+async function serviceWorkerRegistration() {
+	register = await navigator.serviceWorker.register("service-worker.js");
+}
